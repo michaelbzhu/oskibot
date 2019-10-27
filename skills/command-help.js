a0b75d9b63a01a402286fca9b87c2abb90620456
mbzhu1 <mbzhu@airbears2-10-142-129-63.airbears2.1918.berkeley.edu>
2019-10-26 21:05:39 -0700

    deleted tutorial skills

--- skills/command-help.js
+++ skills/command-help.js
@@ -4,8 +4,7 @@
 module.exports = function (controller) {
 
     controller.hears(["help", "who"], 'direct_message,direct_mention', function (bot, message) {
-        var text = "Here are my skills:";
-        text += "\n- " + bot.enrichCommand(message, ".commons") + ": shows metadata about myself";
+        var text = "You can contact Michael Zhu at mbzhu1@berkeley.edu if you have any questions. \n Here are my skills:";
         text += "\n- " + bot.enrichCommand(message, "help") + ": spreads the word about my skills";
         text += "\n- " + bot.enrichCommand(message, "recommend") + ": recommends classes at UC Berkeley for you";
         bot.reply(message, text);
