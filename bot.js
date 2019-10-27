a7251a5f7dcb0aede3dfd788051b61273b41c962
mbzhu1 <mbzhu@airbears2-10-142-129-63.airbears2.1918.berkeley.edu>
2019-10-27 03:20:28 -0700

    successful azure deployment

--- bot.js
+++ bot.js
@@ -3,17 +3,13 @@
 // Licensed under the MIT License 
 //
 
-
-// Load env variables 
-var env = require('node-env-file');
-env(__dirname + '/.env');
-
-
 //
-// BotKit initialization
+// BotKit configuration
 //
 
-var Botkit = require('botkit');
+// Load environment variables from project .env file
+require('node-env-file')(__dirname + '/.env');
+
 
 // Fetch token from environement
 // [COMPAT] supports SPARK_TOKEN for backward compatibility
@@ -21,24 +17,45 @@ var accessToken = process.env.ACCESS_TOKEN || process.env.SPARK_TOKEN
 if (!accessToken) {
     console.log("Could not start as this bot requires a Webex Teams API access token.");
     console.log("Please invoke with an ACCESS_TOKEN environment variable");
+    console.log("Example:");
+    console.log("> ACCESS_TOKEN=XXXXXXXXXXXX PUBLIC_URL=YYYYYYYYYYYYY node bot.js");
+    process.exit(1);
+}
+
+// Get public URL where the Webex cloud platform will post notifications (webhook registration)
+var public_url = process.env.PUBLIC_URL;
+// Infer the app domain for popular Cloud PaaS
+if (!public_url) {
+
+    // Heroku hosting: available if dyno metadata are enabled, https://devcenter.heroku.com/articles/dyno-metadata
+    if (process.env.HEROKU_APP_NAME) {
+        public_url = "https://" + process.env.HEROKU_APP_NAME + ".herokuapp.com";
+    }
+
+    // Glitch hosting
+    if (process.env.PROJECT_DOMAIN) {
+        public_url = "https://" + process.env.PROJECT_DOMAIN + ".glitch.me";
+    }
+}
+if (!public_url) {
+    console.log("Could not start as this bot must expose a public endpoint.");
+    console.log("Please add env variable PUBLIC_URL on the command line or to the .env file");
     console.log("Example: ");
     console.log("> ACCESS_TOKEN=XXXXXXXXXXXX PUBLIC_URL=YYYYYYYYYYYYY node bot.js");
     process.exit(1);
 }
 
-if (!process.env.PUBLIC_URL) {
-    console.log("Could not start as this bot must expose a public endpoint.");
-    console.log("Please add env variable PUBLIC_URL on the command line");
-    console.log("Example: ");
-    console.log("> ACCESS_TOKEN=XXXXXXXXXXXX PUBLIC_URL=YYYYYYYYYYYYY node bot.js");
-    process.exit(1);
-}
+
+//
+// Create bot
+//
+
+var Botkit = require('botkit');
 
 var env = process.env.NODE_ENV || "development";
-
 var controller = Botkit.sparkbot({
     log: true,
-    public_address: process.env.PUBLIC_URL,
+    public_address: public_url,
     ciscospark_access_token: accessToken,
     secret: process.env.SECRET, // this is a RECOMMENDED security setting that checks if incoming payloads originate from Webex
     webhook_name: process.env.WEBHOOK_NAME || ('built with BotKit (' + env + ')')
@@ -47,34 +64,49 @@ var controller = Botkit.sparkbot({
 var bot = controller.spawn({
 });
 
-// Load BotCommons properties
-bot.commons = {};
-bot.commons["healthcheck"] = process.env.PUBLIC_URL + "/ping";
-bot.commons["up-since"] = new Date(Date.now()).toGMTString();
-bot.commons["version"] = "v" + require("./package.json").version;
-bot.commons["owner"] = process.env.owner;
-bot.commons["support"] = process.env.support;
-bot.commons["platform"] = process.env.platform;
-bot.commons["code"] = process.env.code;
 
-// Start Bot API
-controller.setupWebserver(process.env.PORT || 3000, function (err, webserver) {
+//
+// Launch bot
+//
+
+var port = process.env.PORT || 3000;
+controller.setupWebserver(port, function (err, webserver) {
     controller.createWebhookEndpoints(webserver, bot, function () {
-        console.log("webhooks setup successfully");
+        console.log("webhooks setup completed!");
     });
 
     // installing Healthcheck
-    webserver.get('/ping', function (req, res) {
-        res.json(bot.commons);
+    var healthcheck = {
+        "up_since": new Date(Date.now()).toGMTString(),
+        "hostname": require('os').hostname() + ":" + port,
+        "version": "v" + require("./package.json").version,
+        "bot": "unknown",   // loaded asynchronously
+        "botkit": "v" + bot.botkit.version()
+    };
+    webserver.get(process.env.HEALTHCHECK_ROUTE, function (req, res) {
+
+        // As the identity is load asynchronously from the Webex Teams access token, we need to check until it's fetched
+        if (healthcheck.bot == "unknown") {
+            var identity = bot.botkit.identity;
+            if (bot.botkit.identity) {
+                healthcheck.bot = bot.botkit.identity.emails[0];
+            }
+        }
+
+        res.json(healthcheck);
     });
-    console.log("healthcheck available at: " + bot.commons.healthcheck);
+    console.log("healthcheck available at: " + process.env.HEALTHCHECK_ROUTE);
 });
 
+
+//
 // Load skills
+//
+
 var normalizedPath = require("path").join(__dirname, "skills");
 require("fs").readdirSync(normalizedPath).forEach(function (file) {
     try {
-        require("./skills/" + file)(controller);
+        require("./skills/" + file)(controller, bot);
         console.log("loaded skill: " + file);
     }
     catch (err) {
@@ -86,18 +118,28 @@ require("fs").readdirSync(normalizedPath).forEach(function (file) {
     }
 });
 
+
+//
+// Webex Teams Utilities
+//
+
 // Utility to add mentions if Bot is in a 'Group' space
-bot.enrichCommand = function (message, command) {
-    var botName = process.env.BOT_NICKNAME || "BotName";
-    if ("group" == message.roomType) {
+bot.appendMention = function (message, command) {
+
+    // if the message is a raw message (from a post message callback such as bot.say())
+    if (message.roomType && (message.roomType == "group")) {
+        var botName = bot.botkit.identity.displayName;
         return "`@" + botName + " " + command + "`";
     }
-    if (message.original_message) {
-        if ("group" == message.original_message.roomType) {
-            return "`@" + botName + " " + command + "`";
-        }
+
+    // if the message is a Botkit message
+    if (message.raw_message && (message.raw_message.data.roomType == "group")) {
+        var botName = bot.botkit.identity.displayName;
+        return "`@" + botName + " " + command + "`";
     }
 
     return "`" + command + "`";
 }
 
+// [COMPAT] Adding this function to ease interoperability with the skills part of the Botkit samples project
+bot.enrichCommand = bot.appendMention;
